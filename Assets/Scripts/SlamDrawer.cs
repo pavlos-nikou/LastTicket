c280c5b0a8c31fdf2a531e04600fb529b2a0b7ce
Pavlos Nikou <pavlosn.pn@gmail.com>
2026-05-10 00:32:33 +0300

    removed the sounds from drawers

--- Assets/Scripts/SlamDrawer.cs
+++ Assets/Scripts/SlamDrawer.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using UnityEngine;
 public class DrawerSlam : MonoBehaviour
 {
-    public float slamDistance = 0.5f;
+    public float slamDistance = 0.4f;
     public float openSpeed = 1f;
     public float slamSpeed = 10f;
 
     private Vector3 startPos;
     private Coroutine slamCoroutine;
+    public AudioSource audioSource;
+    public AudioClip[] sounds;
+
+    public float minDelay = 3f;
+    public float maxDelay = 10f;
 
     private void Start()
     {
@@ -19,6 +24,7 @@ public class DrawerSlam : MonoBehaviour
         if (slamCoroutine == null)
         {
             slamCoroutine = StartCoroutine(SlamRoutine());
+            slamCoroutineSounds = StartCoroutine(PlayRandomSounds());
         }
     }
 
@@ -27,44 +33,55 @@ public class DrawerSlam : MonoBehaviour
         if (slamCoroutine != null)
         {
             StopCoroutine(slamCoroutine);
+            StopCoroutine(slamCoroutineSounds);
             slamCoroutine = null;
+            slamCoroutineSounds = null;
         }
     }
 
     IEnumerator SlamRoutine()
-{
-    while (true)
     {
-        float waitTime = Random.Range(1f, 3f);
-        yield return new WaitForSeconds(waitTime);
-
-        Vector3 openPos = startPos + transform.forward * slamDistance;
-
-        while (Vector3.Distance(transform.position, openPos) > 0.01f)
+        while (true)
         {
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                openPos,
-                openSpeed * Time.deltaTime
-            );
+            float waitTime = Random.Range(1f, 3f);
+            yield return new WaitForSeconds(waitTime);
 
-            yield return null;
+            Vector3 openPos = startPos + transform.forward * slamDistance;
+
+            while (Vector3.Distance(transform.position, openPos) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    openPos,
+                    openSpeed * Time.deltaTime
+                );
+
+                yield return null;
+            }
+
+            transform.position = openPos;
+
+            while (Vector3.Distance(transform.position, startPos) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    startPos,
+                    slamSpeed * Time.deltaTime
+                );
+                yield return null;
+            }
+            transform.position = startPos;
         }
-
-        transform.position = openPos;
-
-        while (Vector3.Distance(transform.position, startPos) > 0.01f)
-        {
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                startPos,
-                slamSpeed * Time.deltaTime
-            );
-
-            yield return null;
-        }
-
-        transform.position = startPos;
     }
-}
+
+    IEnumerator PlayRandomSounds()
+    {
+        while (true)
+        {
+            // Wait random time
+            float waitTime = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(waitTime);
+            gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+    }
 }
